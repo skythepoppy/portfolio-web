97d948cd44d77e70f450aead20e54063997fcc4a
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-21 14:27:14 -0500

    updated project data

--- pages/Projects.py
+++ pages/Projects.py
@@ -6,7 +6,7 @@ col3, empty_col, col4 = st.columns([1.5 , 0.5 , 1.5])
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
-    for index, row in df[:4].iterrows():
+    for index, row in df[:5].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
@@ -14,7 +14,7 @@ with col3:
 
 
 with col4:
-    for index, row in df[4:].iterrows():
+    for index, row in df[5:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
